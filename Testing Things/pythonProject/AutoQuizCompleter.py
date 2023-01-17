f13377469a52d3ed116b0602a8d500da2c239697
Peter Walkwitz <walkwitzpeter@gmail.com>
2023-01-16 20:26:33 -0600

    threading

--- Testing Things/pythonProject/AutoQuizCompleter.py
+++ Testing Things/pythonProject/AutoQuizCompleter.py
@@ -1,3 +1,4 @@
+import threading
 import time
 
 from bs4 import BeautifulSoup
@@ -17,6 +18,10 @@ This program should complete 10 quizzes for you,
  HOWEVER you will have to answer the captcha at the end of each quiz. 
  After you answer the captcha you will need to enter a user input into the program
  (I run the program in PyCharm and so I just enter it in the PyCharm terminal)
+ 
+ It also provides the option to do all 10 quizzes simultaneously, this doesn't work
+ every single time but it usually gets at least 8 of them all the way to the end.
+ If you want to do all 10 at once flip the "THREADED" option to "True"
 
 This was written for myself so it is kinda messy and not always easy to figure out where
  it fails. But it works perfect for me, so write one yourself to practice if this doesn't
@@ -30,7 +35,7 @@ Things that stop it from working:
     Navigating to a different part of the website at any point (except for clicking the captcha)
 
 Author: Peter Walkwitz
-Date Last Modified: (7/3/2022)
+Date Last Modified: (1/16/2023)
 """
 
 USERNAME = "Username"
@@ -38,6 +43,7 @@ PASSWORD = "Password"
 WIZURL = "https://www.wizard101.com/game"
 # Starts at 0 not 1
 STARTING_QUIZ = 0
+THREADED = True
 
 tenTriviaNames = ['Ancient Egypt Trivia', 'Early American History Trivia', 'Famous World Leaders',
                   'Greek Mythology Trivia', 'Norse Mythology Trivia', 'Solar System Trivia', 'State Nicknames Trivia',
@@ -46,13 +52,14 @@ tenTriviaNames = ['Ancient Egypt Trivia', 'Early American History Trivia', 'Famo
 
 def checkCaptcha(messageString):
     playsound("DoneDing.mp3")
-    userInput = input(messageString)
+    input(messageString)
 
 
-def getCrowns():
+def getCrowns(quizNumber=None):
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(WIZURL)
     print()
+    print(f'Get Crowns - {quizNumber}')
 
     # Process to Log in
     try:
@@ -65,48 +72,52 @@ def getCrowns():
         # Clicking Login
         login_button = driver.find_element(By.XPATH, "//*[@id='wizardLoginButton']/tbody/tr/td[1]/div/div/input")
         login_button.click()
-        checkCaptcha("Logged In? ")
 
     except:
-        print("failed to find login")
+        input('Failed to login ')
 
-    for quizNumber in range(STARTING_QUIZ, 10):
-        quizName = tenTriviaNames[quizNumber]
-        driver.get("https://www.wizard101.com/quiz/trivia/game/" + QuizLinks.QuizLinksDict[quizName])
-
-        # Solving quizzes
-        for questionNumber in range(12):
-            try:
-                content = driver.page_source
-                soup = BeautifulSoup(content)
-                answerQuizQuestion(soup, driver, QuizAnswers.ArrayOfDictionaries[quizNumber])
-            # In case the connection drops, trys to refresh the page and keep it going
-            except AttributeError:
-                driver.refresh()
-                questionNumber -= 1
-                print("Question Number: " + questionNumber)
-
-        # Claiming the crowns
-        claimRewardButton = driver.find_element(By.CLASS_NAME, "kiaccountsbuttongreen")
-        claimRewardButton.click()
-        checkCaptcha("Clicked Captcha? ")
-
+    if THREADED:
+        solveQuiz(driver, quizNumber),
+    else:
+        for quiz in range(10):
+            solveQuiz(driver, quiz)
         # Taking another quiz
         takeAnotherQuizButton = driver.find_element(By.CLASS_NAME, "kiaccountsbuttongreen")
         takeAnotherQuizButton.click()
 
     print("All done!")
-    done = False
-    while not done:
-        time.sleep(10)
+
+
+def solveQuiz(driver, quizNumber):
+    quizName = tenTriviaNames[quizNumber]
+    driver.get("https://www.wizard101.com/quiz/trivia/game/" + QuizLinks.QuizLinksDict[quizName])
+
+    # Solving quizzes
+    for questionNumber in range(12):
+        try:
+            content = driver.page_source
+            soup = BeautifulSoup(content)
+            answerQuizQuestion(soup, driver, QuizAnswers.ArrayOfDictionaries[quizNumber])
+        # In case the connection drops, trys to refresh the page and keep it going
+        except AttributeError:
+            driver.refresh()
+            questionNumber -= 1
+            print("Question Number: " + str(questionNumber))
+            content = driver.page_source
+            soup = BeautifulSoup(content)
+            answerQuizQuestion(soup, driver, QuizAnswers.ArrayOfDictionaries[quizNumber])
+
+    # Claiming the crowns
+    claimRewardButton = driver.find_element(By.CLASS_NAME, "kiaccountsbuttongreen")
+    claimRewardButton.click()
+    checkCaptcha("Clicked Captcha? ")
 
 
 def answerQuizQuestion(soup, driver, dictionary):
     # This grabs the quiz question
     webQuestion = soup.find('div', attrs={'class': 'quizQuestion'})
     # Creating a wait time because the answers fade in
-    # TODO I should probably change this to (when interactable)
-    time.sleep(5)
+    time.sleep(6)
 
     # Creating Answer here, so I can reference outside the for loop
     answer = ""
@@ -114,7 +125,8 @@ def answerQuizQuestion(soup, driver, dictionary):
     # Looping through the dictionary to find the right answer
     for quizQuestion in dictionary:
         if webQuestion.__contains__(quizQuestion):
-            print("answer found: " + quizQuestion)
+            if not THREADED:
+                print("Quiz Question: " + quizQuestion)
             answer = dictionary[quizQuestion]
             break
 
@@ -128,12 +140,12 @@ def answerQuizQuestion(soup, driver, dictionary):
             checkBox += 1
 
         if webAnswer.text.__contains__(answer):
-            print("the real answer is: " + answer)
-            print("finding answer button, webanswer: " + webAnswer.text)
+            if not THREADED:
+                print("the real answer is: " + answer)
+                print("finding answer button, webanswer: " + webAnswer.text)
             answerButton.click()
             break
         else:
-            print("didnt find answer, clicking last option")
             answerButton.click()
 
     # Moving on to the next question
@@ -145,8 +157,17 @@ def answerQuizQuestion(soup, driver, dictionary):
 
 
 if __name__ == '__main__':
-    getCrowns()
 
+    if THREADED:
+        for quizNum in range(STARTING_QUIZ, 10):
+            threading.Thread(target=getCrowns, args=(quizNum,), daemon=True).start()
+            time.sleep(1)
+    else:
+        getCrowns()
+
+    # This is here in case you want to look around the website after program is done
+    while True:
+        time.sleep(10)
 
 # Sources
 # https://www.youtube.com/watch?v=f7LEWxX4AVI
